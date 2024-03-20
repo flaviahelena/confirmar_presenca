98aeb2c6bf08d4a50acfc1f245b1310b93d6a81c
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2024-03-19 22:05:10 -0300

    visual update

---
+++ js/mains.js
@@ -0,0 +1,21 @@
+function enviarConfirmacao(){
+
+}
+
+function abreOpcaoConfirmacao(){
+    var botao = document.getElementById('botao_opcao');
+
+    botao.addEventListener('click', function() {
+        alert('Botão clicado!');
+    });
+
+    // Recupera todos os botões de opção pelo nome
+    /*var botoesOpcao = document.querySelector('input[name="opcao"]');
+        
+    // Adiciona um ouvinte de evento para cada botão de opção
+    botoesOpcao.forEach(function(botao) {
+        botao.addEventListener('change', function() {
+            alert('Opção selecionada: ' + this.value);
+        });
+    });*/
+}
