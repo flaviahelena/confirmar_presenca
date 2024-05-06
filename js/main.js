389dce5fae5e56ee0b3572f6a12ec078202165ac
Flavia Helena da Silva <flaviahsilcor@gmail.com>
2024-05-06 20:32:05 -0300

    ajuste

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 function enviarConfirmacao(){
 
-    //var telefone= "5531986042241";
-    var telefone= "5531988443535";
+    var telefone= "5531986042241";
+    //var telefone= "5531988443535";
     var nome = document.getElementById("dado1").value;
     var url = "https://api.whatsapp.com/send?phone="+telefone+"&text=Ol%C3%A1!%20Gostaria%20de%20confirmar%20a%20presen%C3%A7a%20de%20"+nome+"%20para%20o%20casamento%20de%20Fernanda%20e%20Rodrigo!"
     
